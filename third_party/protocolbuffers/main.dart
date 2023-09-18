b2dc7c3db129997f9a01b9441aeab37c9e64f77a
Liangcheng Juves <email@lcjuves.com>
2023-09-18 21:11:46 +0800

    Sort `bookNameList` before generating `books.pb`

--- third_party/protocolbuffers/main.dart
+++ third_party/protocolbuffers/main.dart
@@ -6,6 +6,7 @@ import 'dart:io';
 Future main(List<String> args) async {
   final booksJson = await File('../files/json/books.json').readAsString();
   final bookNameList = List<String>.from(jsonDecode(booksJson));
+  bookNameList.sort((a, b) => a.toLowerCase().compareTo(b.toLowerCase()));
   final books = Books(bookNameList: bookNameList);
   final booksPB = File('../../books.pb');
   await booksPB.writeAsBytes(books.writeToBuffer(), flush: true);
