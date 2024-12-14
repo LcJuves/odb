5f8802139c97923e0004aa3d03f1dceb8643df20
Liangcheng Juves <email@lcjuves.com>
2024-12-14 23:41:43 +0800

    Optimize the rendering performance of the web version of PDF on desktop platforms, and do not use the embed element to render PDF when opening the web version on mobile devices

--- lib/webspec.dart
+++ lib/webspec.dart
@@ -1,21 +1,39 @@
 import 'package:web/web.dart';
 
 Future<void> webLoadAnimationAndLaunch(String url) async {
-  var elem = document.querySelector(".content") as HTMLDivElement?;
+  final ua = window.navigator.userAgent.toLowerCase();
+  final isAndroidWebViewOrBrowser = ua.contains("android");
+  final isIOSWebViewOrBrowser = RegExp(r'iphone|ipad|ipod|ios').hasMatch(ua);
+
+  final content = document.querySelector(".content") as HTMLDivElement?;
+  content?.style.display = 'flex';
   final embed = document.createElement("embed") as HTMLEmbedElement;
-  elem?.style.display = 'flex';
-  embed.type = "application/pdf";
-  embed.src = url;
-  embed.style.position = "absolute";
-  embed.style.width = "100%";
-  embed.style.height = "100%";
-  embed.style.overflow = "hidden";
-  embed.style.display = "none";
-  // TODO: Cancel to hidden embed pdf
-  // window.history.pushState(null, "", url);
-  document.body!.appendChild(embed);
+  if (!(isAndroidWebViewOrBrowser || isIOSWebViewOrBrowser)) {
+    embed.type = "application/pdf";
+    embed.src = url;
+    embed.style.position = "absolute";
+    embed.style.width = "100%";
+    embed.style.height = "100%";
+    embed.style.overflow = "hidden";
+    embed.style.display = "none";
+    // TODO: Cancel to hidden embed pdf
+    // window.history.pushState(null, "", url);
+    document.body!.appendChild(embed);
+  }
+
+  if (isAndroidWebViewOrBrowser || isIOSWebViewOrBrowser) {
+    window.location.replace(url);
+    return;
+  }
   await Future.delayed(const Duration(seconds: 6), () {
-    embed.style.display = 'block';
-    elem?.style.display = 'none';
+    if (!(isAndroidWebViewOrBrowser || isIOSWebViewOrBrowser)) {
+      final flutterView =
+          document.querySelector("flutter-view") as HTMLDivElement?;
+      flutterView?.remove();
+    }
+    content?.style.display = 'none';
+    if (!(isAndroidWebViewOrBrowser || isIOSWebViewOrBrowser)) {
+      embed.style.display = 'block';
+    }
   });
 }
